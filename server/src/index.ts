d40833ee5f24a0fd941bbf85949698612432443c
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-09-07 01:51:09 +0200

    Graphql and apollo setup, posts crud

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
+import "reflect-metadata";
 import { MikroORM, RequestContext } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import config from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(config);
@@ -16,10 +19,19 @@ const main = async () => {
 
     const generator = orm.getSchemaGenerator();
     await generator.updateSchema();
-
-    const post = orm.em.create(Post, { title: "my first post" });
-    await orm.em.persistAndFlush(post);
   });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen("4000", () => {
     console.log("server started on localhost:4000");
